cf368b7898a88d2866ebc298e50b8aa02c4847ca
David Beckley <beckl.d.413@isg.edu.sa>
2012-11-16 15:45:14 +0300

    Added multi-file abilities

--- assembler.py
+++ assembler.py
@@ -5,14 +5,64 @@ import re
 import time
 import threading
 
-def assemble_binary(code):
+def link(files):
+    out_fd, out_fname = tempfile.mkstemp()
+    os.fdopen(out_fd).close()
+
+    process_flags = ["dtld", "-o", out_fname, "-k", "none"]
+    filenames = []
+    for file in files:
+        fd, fname = tempfile.mkstemp()
+	f = os.fdopen(fd, 'wb')
+	f.write(file)
+	f.close()
+        filenames.append(fname)
+        process_flags.append(fname)
+
+    proc = subprocess.Popen(process_flags, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    res, err = proc.communicate()
+
+    for file in filenames:
+        os.remove(file)
+
+    final = open(out_fname)
+    res = final.read()
+    final.close()
+
+    return (res, err)
+
+def assemble_file(code, binary=False):
     code = '\n'.join(code.split('/'))
     code = '\n'.join(code.split('\\'))
-    process = subprocess.Popen(["dtasm", "-o", "-", "--binary", "-"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    process_flags = ["dtasm", "-o", "-", "-"]
+    if binary:
+        process_flags.append("--binary")
+    process = subprocess.Popen(process_flags, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     results = process.communicate(code)
     res = results[0]
     err = results[1]
     return (res, err)
+
+def assemble_binary(code):
+
+    code = '///'.join(code.split('\\\\\\'))
+    files = code.split('///')
+
+    if len(files) > 1:
+        file_binaries = []
+	err = ""
+        for file in files:
+            assembled = assemble_file(file)
+            if assembled[0]:
+                file_binaries.append(assembled[0])
+            if assembled[1]:
+                err += assembled[1]
+        res, link_err = link(file_binaries)
+        err += link_err
+	return (res, err)
+    else:
+        return assemble_file(code, True)
 
 def assemble(code):
     res, err = assemble_binary(code)
@@ -21,10 +71,10 @@ def assemble(code):
     while i < len(res) /  2:
         byte_one = ord(res[2*i])
         byte_two = ord(res[2*i+1])
-	print "Bytes:", byte_one, byte_two
+        print "Bytes:", byte_one, byte_two
         word = "0x%04x" % ((byte_one << 8) + byte_two)
         words.append(word)
-	i += 1
+        i += 1
     print "Assembly attempted"
     return (words, err)
 
@@ -36,7 +86,7 @@ register_re = re.compile(r"([A-Z]{1,2}):\s*0x([\dA-F]+)")
 
 def execute(code):
     binary, err = assemble_binary(code)
-    if err != "" and not ("warning" in err):
+    if err and not ("warning" in err):
         return err
 
     num_words = len(binary) / 2
