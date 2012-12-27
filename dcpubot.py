c895a9d79c75d8c63e0a175c209ca0e14ae80397
David Beckley <beckl.d.413@isg.edu.sa>
2012-12-27 11:00:34 +0300

    Reverted commit

--- dcpubot.py
+++ dcpubot.py
@@ -5,112 +5,115 @@ import subprocess
 import dcpu
 import random
 
-def onConnect():
-  irc.join(config.chan)
+irc.connect(config.host, config.port, config.nick, config.password)
+irc.join(config.chan)
 
-  def onAssemble(nick, user, host, chan, matches):
-      print "Assembling"
-      print matches.group()
-      print matches.group(1)
-      binary, errors = dcpu.assemble(matches.group(1))
+def onAssemble(nick, user, host, chan, matches):
+    print "Assembling"
+    print matches.group()
+    print matches.group(1)
+    binary, errors = dcpu.assemble(matches.group(1))
 
-      if binary:
-          irc.privmsg(nick, chan, ', '.join(binary))
-      if errors:
-          irc.privmsg(nick, chan, errors)
+    if binary:
+        irc.privmsg(nick, chan, ', '.join(binary))
+    if errors:
+        irc.privmsg(nick, chan, errors)
 
-  irc.onPrivmsg(">>>(.+)", onAssemble)
+irc.onPrivmsg(">>>(.+)", onAssemble)
 
-  def onDisassemble(nick, user, host, chan, matches):
-      print "Disassembling"
-      print matches.group()
-      print matches.group(1)
-      code = dcpu.disassemble(matches.group(1))
+def onDisassemble(nick, user, host, chan, matches):
+    print "Disassembling"
+    print matches.group()
+    print matches.group(1)
+    code = dcpu.disassemble(matches.group(1))
 
-      if code:
-          irc.privmsg(nick, chan, code)
+    if code:
+        irc.privmsg(nick, chan, code)
 
-  irc.onPrivmsg("<<<(.+)", onDisassemble)
+irc.onPrivmsg("<<<(.+)", onDisassemble)
 
-  def onExecute(nick, user, host, chan, matches):
-      executed, errors = dcpu.execute(matches.group(1))
+def onExecute(nick, user, host, chan, matches):
+    executed, errors = dcpu.execute(matches.group(1))
 
-      if executed:
-          irc.privmsg(nick, chan, executed)
-      if errors:
-          irc.privmsg(nick, chan, errors)
+    if executed:
+        irc.privmsg(nick, chan, executed)
+    if errors:
+        irc.privmsg(nick, chan, errors)
 
-  irc.onPrivmsg(">>([^>].+)", onExecute)
+irc.onPrivmsg(">>([^>].+)", onExecute)
 
-  def onHex(nick, user, host, chan, matches):
-      converted = 0
-      
-      if matches.group(1) == "0b":
-          converted = hex(int(matches.group(2), 2))
-      else:
-          converted = hex(int(matches.group(2)))
+def onHex(nick, user, host, chan, matches):
+    converted = 0
+    
+    if matches.group(1) == "0b":
+        converted = hex(int(matches.group(2), 2))
+    else:
+        converted = hex(int(matches.group(2)))
 
-      irc.privmsg(nick, chan, converted)
+    irc.privmsg(nick, chan, converted)
 
-  irc.onPrivmsg(r"^hex\((0b)?(\d+)\)", onHex)
+irc.onPrivmsg(r"^hex\((0b)?(\d+)\)", onHex)
 
-  def onDec(nick, user, host, chan, matches):
-      converted = 0
-      
-      if matches.group(1) == "0b":
-          converted = str(int(matches.group(2), 2))
-      elif matches.group(1) == "0x":
-          converted = str(int(matches.group(2), 16))
-      else:
-          converted = str(int(matches.group(2)))
+def onDec(nick, user, host, chan, matches):
+    converted = 0
+    
+    if matches.group(1) == "0b":
+        converted = str(int(matches.group(2), 2))
+    elif matches.group(1) == "0x":
+        converted = str(int(matches.group(2), 16))
+    else:
+        converted = str(int(matches.group(2)))
 
-      irc.privmsg(nick, chan, converted)
+    irc.privmsg(nick, chan, converted)
 
-  irc.onPrivmsg(r"^dec\((0b|0x)?([0-9a-fA-F]+)\)", onDec)
+irc.onPrivmsg(r"^dec\((0b|0x)?([0-9a-fA-F]+)\)", onDec)
 
-  def onBin(nick, user, host, chan, matches):
-      converted = 0
+def onBin(nick, user, host, chan, matches):
+    converted = 0
 
-      if matches.group(1) == "0x":
-          converted = bin(int(matches.group(2), 16))
-      else:
-          converted = bin(int(matches.group(2), 16))
+    if matches.group(1) == "0x":
+        converted = bin(int(matches.group(2), 16))
+    else:
+        converted = bin(int(matches.group(2), 16))
 
-      irc.privmsg(nick, chan, converted)
+    irc.privmsg(nick, chan, converted)
 
-  irc.onPrivmsg(r"^bin\((0x)?([0-9a-fA-F]+)\)", onBin)
+irc.onPrivmsg(r"^bin\((0x)?([0-9a-fA-F]+)\)", onBin)
 
-  def onStinks(nick, user, host, chan, matches):
-      messages = ["So do you!!!", "Shut up.", "You smell even worse.", "You really shouldn't be talking."]
-      irc.privmsg(nick, chan, choice(messages))
+def onStinks(nick, user, host, chan, matches):
+    messages = ["So do you!!!", "Shut up.", "You smell even worse.", "You really shouldn't be talking."]
+    irc.privmsg(nick, chan, choice(messages))
 
-  irc.onPrivmsg(".*" + config.nick + r":?( ?is| you)? stink(ing|s)?.*", onStinks)
+irc.onPrivmsg(".*" + config.nick + r":?( ?is| you)? stink(ing|s)?.*", onStinks)
 
-  def onReload(nick, user, host, chan, matches):
-      if(host == "unaffiliated/thatotherpersony"):
-          subprocess.call(["git", "pull", "origin", "master"]);
-          irc.privmsg(nick, chan, "Pulled latest changes from GitHub. Attempting to reload.")
-          irc.reload()
-      elif(host == "unaffiliated/quu"):
-          irc.privmsg(nick, chan, "wat. Quu. derp.\nReally?\nInitializing spambot mode. >:D")
-      else:
-          irc.privmsg(nick, chan, "No. I don't wanna!")
+def onReload(nick, user, host, chan, matches):
+    if(host == "unaffiliated/thatotherpersony"):
+        subprocess.call(["git", "pull", "origin", "master"]);
+        irc.privmsg(nick, chan, "Pulled latest changes from GitHub. Attempting to reload.")
+        irc.reload()
+    elif(host == "unaffiliated/quu"):
+        irc.privmsg(nick, chan, "wat. Quu. derp.\nReally?\nInitializing spambot mode. >:D")
+    else:
+        irc.privmsg(nick, chan, "No. I don't wanna!")
 
-  irc.onMsgToMe(".*reload.*", onReload)
+irc.onMsgToMe(".*reload.*", onReload)
 
-  def onRudeness(nick, user, host, chan, matches):
-      irc.privmsg(nick, chan, "Why don't you?")
+def onTest(nick, user, host, chan, matches):
+    irc.privmsg(nick, chan, "lolololol wat derp. ping Quu! ping mrout! Why not!")
 
-  irc.onMsgToMe(".*stfu.*", onRudeness)
+irc.onMsgTome(".*test.*", onTest)
 
-  def onHello(nick, user, host, chan, matches):
-      irc.privmsg(nick, chan, "Howdy!")
+def onRudeness(nick, user, host, chan, matches):
+    irc.privmsg(nick, chan, "Why don't you?")
 
-  irc.onMsgToMe(".*hello.*", onHello)
+irc.onMsgToMe(".*stfu.*", onRudeness)
 
-  def onSup(nick, user, host, chan, matches):
-      irc.privmsg(nick, chan, "I'm fine. How about you?")
+def onHello(nick, user, host, chan, matches):
+    irc.privmsg(nick, chan, "Howdy!")
 
-  irc.onMsgToMe(".*(how.*you|sup|what.*up).*", onSup)
+irc.onMsgToMe(".*hello.*", onHello)
 
-irc.connect(onConnect, config.host, config.port, config.nick, config.password)
+def onSup(nick, user, host, chan, matches):
+    irc.privmsg(nick, chan, "I'm fine. How about you?")
+
+irc.onMsgToMe(".*(how.*you|sup|what.*up).*", onSup)
--- irc.py
+++ irc.py
@@ -1,5 +1,6 @@
 import socket
 import re
+import threading
 import sys
 
 command_handlers = []
@@ -12,11 +13,26 @@ def command(command, params):
     print msg
     server.sendall(msg + "\r\n")
 
+def reloadFunction(function):
+    old = sys.modules[handler.__module__]
+    new = reload(old)
+    function = getattr(new, function.__name__)
+    return function
+
 def reload():
-    global callback
-    module = reload(sys.modules[callback.__module__])
-    callback = getattr(module, callback.__name__)
-    handleEvents(callback)
+    for i, handler in enumerate(command_handlers):
+        command_handlers[i] = reloadFunction(handler)
+
+    for i, handler in enumerate(privmsg_handlers):
+        privmsg_handlers[i] = reloadFunction(handler)
+
+    for i, handler in enumerate(msgtome_handlers):
+        msgtome_handlers[i] = reloadFunction(handler)
+
+    new = reload(sys.modules[__name__])
+
+    for attr in dir(sys.modules[__name__]):
+        setattr(self, attr, getattr(new, attr))
 
 def privmsg(nickIn, chan, msg):
 
@@ -55,21 +71,19 @@ def handlePrivmsg(nickIn, user, host, chan, params):
     if matches:
         handleMsgToMe(nickIn, user, host, chan, matches.group(1))
 
-def handleEvents(call):
-    call()
-    global callback
-    callback = call
-    while True:
-        try:
-            message = server.recv(4096)
-            if message == '':
-                server.close()
+class EventHandler(threading.Thread):
+    def run(self):
+        while True:
+            try:
+                message = server.recv(4096)
+                if message == '':
+                    server.close()
 
-            handleCommand(message)
-        except Exception as e:
-           print e.message
+                handleCommand(message)
+            except Exception as e:
+               print e.message
 
-def connect(callback, host, port, nickIn="TestBot", password="", name="dcpubot", realname="DCPU Bot"):
+def connect(host, port, nickIn="TestBot", password="", name="dcpubot", realname="DCPU Bot"):
     global nick
     nick = nickIn
     
@@ -83,7 +97,8 @@ def connect(callback, host, port, nickIn="TestBot", password="", name="dcpubot",
     onCommand('PING', handlePing)
     onCommand('PRIVMSG', handlePrivmsg)
 
-    handleEvents(callback)
+    eventHandler = EventHandler()
+    eventHandler.start()
 
 def join(channels):
     print channels
